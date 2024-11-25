75f4e4fab9c0e8cd8992b7a6323f313842bedcfc
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-11-25 21:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+const input = document.getElementById("fname");
+
+input.addEventListner("change",()=>{
+	const text = input.textContent;
+	input.textContent = text.toUpperCase();
+})
